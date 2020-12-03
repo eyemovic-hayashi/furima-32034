89e825a9a5d4aa21687287a8c5c385b3fe1252b4
mokomoka0733 <maocq10016@gmail.com>
2020-12-03 22:04:40 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :price, :category_id, :status_id, :delivery_fee_id, :from_area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
 end
