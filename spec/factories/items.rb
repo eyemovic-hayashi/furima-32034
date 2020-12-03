9b7411de775ed7bb1af53ba1e13ef0e7f1382268
mokomoka0733 <maocq10016@gmail.com>
2020-12-04 00:23:34 +0900

    itemモデルテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name            { Faker::Commerce.product_name }
+    explanation         { Faker::Lorem.sentence }
+    category_id     { 2 }
+    status_id    { 2 }
+    delivery_fee_id { 2 }
+    from_area_id   { 2 }
+    delivery_day_id       { 2 }
+    price           { Faker::Commerce.price(range: 300..10_000).to_i }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品ができるとき' do
+      it 'image, item_name, explanation, category_id, status_id, delivery_fee_id, from_area_id, delivery_day_id, price, user_idがあれば商品出品に成功する' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ができないとき' do
+      it 'imageが空だと商品出品に失敗する' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空だと商品出品に失敗する' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'explanationが空だと商品出品に失敗する' do
+        @item.explanation = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'category_idを選択していないと商品出品に失敗する' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+      it 'status_idを選択していないと商品出品に失敗する' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status Select')
+      end
+      it 'delivery_fee_idを選択していないと商品出品に失敗する' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery fee Select')
+      end
+      it 'from_area_idを選択していないと商品出品に失敗する' do
+        @item.from_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('From area Select')
+      end
+      it 'period_idを選択していないと商品出品に失敗する' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery day Select')
+      end
+      it 'priceが空だと商品出品に失敗する' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300未満だと商品出品に失敗する' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが9,999,999より大きいと商品出品に失敗する' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '出品する商品に紐づくユーザーがいないとき商品出品に失敗する' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
