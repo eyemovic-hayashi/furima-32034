4c90d62f8184bb37b58c2010051f497c658b1de2
mokomoka0733 <maocq10016@gmail.com>
2020-12-07 21:34:32 +0900

    バリデーション追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,6 +9,8 @@ class PurchaseAddress
     validates :address_number
     validates :phone_number, format: { with: /\A\d{1,11}\z/, message: 'Input only number' }
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   def save
