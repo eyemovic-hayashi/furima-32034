dfbb199a94a5e7b69e9a8d7ad332cf592c493c94
mokomoka0733 <maocq10016@gmail.com>
2020-12-07 19:21:16 +0900

    購入機能実装

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -7,8 +7,10 @@ class PurchaseController < ApplicationController
   end
 
   def create
+    # binding.pry
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -23,6 +25,15 @@ class PurchaseController < ApplicationController
   end
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_number, :from_area_id, :municipalities, :address_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:post_number, :from_area_id, :municipalities, :address_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+        Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+        Payjp::Charge.create(
+          amount: @item.price,
+          card: purchase_params[:token],
+          currency: 'jpy'
+        )
   end
 end
